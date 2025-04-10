586b969b4d403ae26da9f5152efcd1c08ea1fd40
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-04-10 20:21:04 +0900

    add:userにvalidationを追加しました。また、ログイン新規登録画面のロゴをhomeにリンクしました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :bio, length: { maximum: 200 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
 end
