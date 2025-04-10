39fa882be4f75eef3f0c63958397cb2042d46495
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-04-10 13:33:27 +0900

    add:Userテーブルにログイン失敗回数上限関係のカラムと、name,introduction,isguestを追加

--- db/migrate/20250410040347_devise_create_users.rb
+++ db/migrate/20250410040347_devise_create_users.rb
@@ -28,10 +28,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.2]
       # t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
+      t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      t.string   :unlock_token # Only if unlock strategy is :email or :both
+      t.datetime :locked_at
 
+
+      t.string :name, null: false
+      t.string :introduction
+      t.boolean :is_guest, default: false, null: false
 
       t.timestamps null: false
     end
@@ -39,6 +43,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.2]
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :users, :unlock_token,         unique: true
   end
 end
