7903d4d28645a6aa8b70fe51e2e54bb1d62e5892
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-04-10 20:20:12 +0900

    add:ログインや新規登録後にログインを確認できる画面に移行するようにしました。

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def user_check
+    if user_signed_in?
+      @user = current_user
+    else
+      @user = nil
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
   root "static_pages#home"
+  get "user_check" => "static_pages#user_check"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
