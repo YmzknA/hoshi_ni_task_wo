f3935dcb2453609745fbdb1fe2bb438586bc2179
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-04-11 00:10:14 +0900

    add:userモデルに必要メソッドを追加し、userテーブルにuuidとproviderカラムを追加しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,31 @@ class User < ApplicationRecord
   validates :bio, length: { maximum: 200 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
+
+  def social_profile(provider)
+    social_profiles.select { |sp| sp.provider == provider.to_s }.first
+  end
+
+  # rubocop:disable Naming/AccessorMethodName
+  def set_values(omniauth)
+    return if provider.to_s != omniauth["provider"].to_s || uid != omniauth["uid"]
+
+    credentials = omniauth["credentials"]
+    info = omniauth["info"]
+
+    # rubocop:disable Lint/UselessAssignment
+    access_token = credentials["refresh_token"]
+    access_secret = credentials["secret"]
+    credentials = credentials.to_json
+    name = info["name"]
+    # rubocop:enable Lint/UselessAssignment
+  end
+
+  # rubocop:disable Style/RedundantSelf
+  def set_values_by_raw_info(raw_info)
+    self.raw_info = raw_info.to_json
+    self.save!
+  end
+  # rubocop:enable Naming/AccessorMethodName
+  # rubocop:enable Style/RedundantSelf
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+   config.hosts << "55c8-123-254-11-172.ngrok-free.app"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
