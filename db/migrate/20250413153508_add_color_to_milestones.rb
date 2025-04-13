2ded52390711726a6965e7b4aa12c8a36f26d62f
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-04-14 00:38:27 +0900

    add:milestonesテーブルにcolorカラムを追加しました

--- db/migrate/20250413153508_add_color_to_milestones.rb
+++ db/migrate/20250413153508_add_color_to_milestones.rb
@@ -1,4 +1,11 @@
 class AddColorToMilestones < ActiveRecord::Migration[7.2]
   def change
+    add_column :milestones, :color, :string, default: "#FFDF5E", null: false
+
+    reversible do |dir|
+      dir.up do
+        Milestone.update_all(color: "#FFDF5E")
+      end
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_13_123214) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_13_153508) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_13_123214) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color", default: "#FFDF5E", null: false
     t.index ["user_id"], name: "index_milestones_on_user_id"
   end
 
