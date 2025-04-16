60b2accf65bd362779a27d6dfe5db90461dad3e4
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-04-16 21:14:17 +0900

    fix:画面遷移時モーダルがキャッシュで残っている問題を修正しました

--- app/javascript/controllers/show_modal_controller.js
+++ app/javascript/controllers/show_modal_controller.js
@@ -2,10 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="show-modal"
 export default class extends Controller {
-  static targets = ["task_modal"]
+  static targets = ["task_modal", "modal_top"]
 
   connect() {
+    this.task_modalTarget.showModal();
   }
 
+  disconnect() {
+    this.modal_topTarget.innerHTML = "";
+  }
 
 }
