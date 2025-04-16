53db7aa4b18f820b3c15f7c27494705822b1a8a8
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-04-16 17:12:15 +0900

    add:show-modalコントローラーに、モーダルが読み込まれた際にshowmodalを実行するコードを記載しました

--- app/javascript/controllers/show_modal_controller.js
+++ app/javascript/controllers/show_modal_controller.js
@@ -2,6 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="show-modal"
 export default class extends Controller {
+  static targets = ["task_modal"]
+
   connect() {
+    console.log("ShowModalController connected")
+    this.task_modalTarget.showModal();
   }
+
+
 }
