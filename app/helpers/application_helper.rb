46b61aaf80c1b34af3842def36c9e7621055bc2b
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-04-16 12:28:37 +0900

    add:ヘルパーにdateインスタンスを渡すと、曜日を日本語で返すメソッドをついかしまいした

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def current_user?(user)
     user && user == current_user
   end
+
+  def day_of_week(date)
+    return if date.nil?
+
+    day_name_ja = %w[日 月 火 水 木 金 土]
+
+    d = date.to_date.wday
+
+    day_name_ja[d]
+  end
 end
