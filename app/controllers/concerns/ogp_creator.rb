4248b037aa014e70f68602b73c3269ce8dd59cb7
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-05-04 16:57:20 +0900

    add: ogpに画像を付与する際の文字と画像のバランスを調整し、bg_imageを更新

--- app/controllers/concerns/ogp_creator.rb
+++ app/controllers/concerns/ogp_creator.rb
@@ -1,45 +1,45 @@
 # frozen_string_literal: true
 
 class OgpCreator
+  # ogp_bgは1200x627の画像
+
   BASE_IMAGE_PATH = "./app/assets/images/ogp_bg.png"
   GRAVITY = "center"
-  TEXT_POSITION = "0,0"
   FONT = "./app/assets/fonts/ZenOldMincho-Regular.ttf"
-  FONT_SIZE = 65
-  INDENTION_COUNT = 16
-  ROW_LIMIT = 8
+  FONT_SIZE = 50
+  INDENTION_COUNT = 10
+  ROW_LIMIT = 5
+  ROUND_IMAGE_POSITION = "+150+168"
+  COLOR = "#EAF2F5"
 
   def self.build(text, image_name = nil)
+    # 三項演算子の時は分かりやすさのために.nil?で分岐してます
+    # image_nameがnilの場合は、centerに配置
+    text_position = "#{image_name.nil? ? '0' : '200'}, -20"
+
     text = prepare_text(text)
-    image = MiniMagick::Image.open(BASE_IMAGE_PATH)
+    base_image = MiniMagick::Image.open(BASE_IMAGE_PATH)
 
     if image_name
-      thumb_path = "./app/assets/images/#{image_name}.webp"
-      thumb_image = MiniMagick::Image.open(thumb_path)
+      # thumb_imageから円形にくりぬき、ボーダーを付与した画像を作成
+      rounded_thumb = rounded_thumb(image_name)
 
-      MiniMagick::Tool::Convert.new do |img|
-        img.size "#{thumb_image.height}x#{thumb_image.width}"
-        img << "xc:transparent"
-        img.fill thumb_path
-        img.draw "translate 600, 600 circle 0,0 600,0"
-        img.trim
-        img << "out_put.png"
-      end
-
-      thumb_image = MiniMagick::Image.open("out_put.png").resize "200x200"
-
-      image = image.composite(thumb_image) do |c|
+      # くりぬいた画像をbase_imageに合成
+      base_image = base_image.composite(rounded_thumb) do |c|
         c.compose "Over"
-        c.geometry "+20+20"
+        c.geometry ROUND_IMAGE_POSITION
+        c.border "5"
+        c.bordercolor "transparent"
       end
     end
 
-    image.combine_options do |config|
+    # base_imageにテキストを合成
+    base_image.combine_options do |config|
       config.font FONT
-      config.fill "red"
+      config.fill COLOR
       config.gravity GRAVITY
       config.pointsize FONT_SIZE
-      config.draw "text #{TEXT_POSITION} '#{text}'"
+      config.draw "text #{text_position} '#{text}'"
     end
   end
 
@@ -47,5 +47,37 @@ class OgpCreator
     text.to_s.scan(/.{1,#{INDENTION_COUNT}}/)[0...ROW_LIMIT].join("\n")
   end
 
-  private_class_method :prepare_text
+  def self.rounded_thumb(image_name)
+    thumb_path = "./app/assets/images/#{image_name}.webp"
+    thumb_image = MiniMagick::Image.open(thumb_path)
+
+    # 画像を丸く切り抜き、画像の周りは白色で塗りつぶす
+    MiniMagick::Tool::Convert.new do |img|
+      img.size "#{thumb_image.height}x#{thumb_image.width}"
+      img << "xc:transparent"
+      img.fill thumb_path
+      img.draw "translate 600, 600 circle 0,0 580,0"
+      img.trim
+      img.border "20x20"
+      img.bordercolor COLOR
+      img << "out_put.png"
+    end
+
+    out_put_image = MiniMagick::Image.open("out_put.png")
+
+    # 上記で作成した画像を一回り大きく円形にくりぬくことで、白いボーダーを付与
+    MiniMagick::Tool::Convert.new do |img|
+      img.size "#{out_put_image.height}x#{out_put_image.width}"
+      img << "xc:transparent"
+      img.fill "out_put.png"
+      img.draw "translate 600, 600 circle 0,0 600,0"
+      img.trim
+      img << "rounded_thumb.png"
+    end
+
+    # くりぬいた画像をbase_imageに合成するために、サイズを250x250にリサイズ
+    MiniMagick::Image.open("rounded_thumb.png").resize "250x250"
+  end
+
+  private_class_method :prepare_text, :rounded_thumb
 end
