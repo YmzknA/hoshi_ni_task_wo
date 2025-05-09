f69bded0b78517bcd95b2af8a087beb1493687f2
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-05-09 13:17:25 +0900

    add: nanoid_generatoeというモデルコンサーンを定義

---
+++ app/models/concerns/nanoid_generator.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module NanoIdGenerator
+  extend ActiveSupport::Concern
+
+  included do
+    before_create :set_id
+  end
+
+  ID_ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  ID_LENGTH = 21
+  MAX_RETRY = 1000
+
+  class_methods do
+    def generate_nanoid(alphabet: ID_ALPHABET, size: ID_LENGTH)
+      Nanoid.generate(size:, alphabet:)
+    end
+  end
+
+  def set_id
+    return if id.present?
+
+    MAX_RETRY.times do
+      self.id = generate_id
+      return unless self.class.exists?(id:)
+    end
+    raise "Failed to generate a unique public id after #{MAX_RETRY} attempts"
+  end
+
+  def generate_id
+    self.class.generate_nanoid
+  end
+end
