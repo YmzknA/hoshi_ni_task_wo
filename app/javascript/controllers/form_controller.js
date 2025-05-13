6322e5d3c68be947cff8dcc8af5322b310ead4c0
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-05-13 19:30:25 +0900

    add: インスタントサーチの実装および、検索ソートパーシャルの切り出し

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -4,4 +4,12 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
   }
+  
+  submit() {
+    clearTimeout(this.timeout)
+
+    this.timeout = setTimeout(() => {
+      this.element.requestSubmit()
+    }, 200)
+  }
 }
