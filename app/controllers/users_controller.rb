9c33b3ce0750575aedfffe9958a12dde89864d2f
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-05-21 12:41:27 +0900

    add: 通知のON/OFF機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by(id: params[:id])
+  before_action :authenticate_user!, except: [:show]
+  before_action :set_user, only: [:show, :toggle_notifications]
+  before_action :validate_another_user, only: [:toggle_notifications]
 
+  def show
     # @userがnilであるか、またはゲストユーザかつ現在のユーザと異なる場合はリダイレクト
     # ゲストユーザーはノイズになるのを避けるために、他者に詳細画面を表示しない
     if @user.nil? || (@user.guest? && !current_user?(@user))
@@ -20,7 +22,36 @@ class UsersController < ApplicationController
     end
   end
 
+  def toggle_notifications
+    if @user.nil?
+      redirect_to root_path, alert: "ユーザが見つかりません"
+    else
+      @user.is_notifications_enabled = !@user.is_notifications_enabled
+
+      if @user.save
+        flash.now[:notice] = if @user.is_notifications_enabled
+                               "通知を受け取るように設定しました"
+                             else
+                               "通知を受け取らないように設定しました"
+                             end
+      else
+        flash.now[:alert] = "通知の切り替えに失敗しました"
+      end
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def validate_another_user
+    return if current_user?(@user)
+
+    flash[:alert] = "他のユーザーの設定は変更できません"
+    redirect_to root_path
+  end
 
   def prepare_meta_tags(user)
     set_meta_tags og: {
