1f6ac9b2f106d8f7b1f05e968cb4401c18467a39
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-05-23 20:17:19 +0900

    fix:ローディングの見た目を変え、表示ロジックを変更

--- app/javascript/controllers/loading_close_controller.js
+++ app/javascript/controllers/loading_close_controller.js
@@ -6,8 +6,10 @@ export default class extends Controller {
   // 接続時、loading_animationを閉じ、loading_ringsを非表示にする
   connect() {
     const loading = document.querySelector("#loading");
-    const loadingRings = document.querySelector("#loading_rings");
     loading.close();
-    loadingRings.classList.add("hidden");
+
+    this.timeout = setTimeout(() => {
+    loading.close();
+    }, 150);
   }
 }
--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -2,21 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="loading"
 export default class extends Controller {
-  static targets = ["loading_animation", "loading_rings"]
-
-  // 接続時、loading_animationを閉じ、loading_ringsを非表示にする
-  connect() {
-    this.loading_animationTarget.close();
-    this.loading_ringsTarget.classList.add("hidden");
-  }
-
+  static targets = ["loading_animation"]
   // ボタンなどにアクションを配置し、loading_animationを開き、loading_ringsを表示する
   // すこしディレイをかけることで、軽いローディングの場合はすこしブラーがかかる程度のエフェクトのみにする
   show() {
-    this.loading_ringsTarget.classList.add("hidden");
-    this.loading_animationTarget.showModal();
     this.timeout = setTimeout(() => {
-      this.loading_ringsTarget.classList.remove("hidden");
-    }, 200);
+      requestAnimationFrame(() => {
+        this.loading_animationTarget.showModal();
+      });
+    }, 150);
   }
 }
