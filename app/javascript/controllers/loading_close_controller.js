ee84519b0a0215d61087136c1e07728c6258c779
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-05-23 18:14:53 +0900

    fix: コメント追加

--- app/javascript/controllers/loading_close_controller.js
+++ app/javascript/controllers/loading_close_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="loading-close"
 export default class extends Controller {
+
+  // 接続時、loading_animationを閉じ、loading_ringsを非表示にする
   connect() {
     const loading = document.querySelector("#loading");
     const loadingRings = document.querySelector("#loading_rings");
--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -2,13 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="loading"
 export default class extends Controller {
-  static targets = ["loading_animation", "loading_top", "loading_rings"]
+  static targets = ["loading_animation", "loading_rings"]
 
+  // 接続時、loading_animationを閉じ、loading_ringsを非表示にする
   connect() {
     this.loading_animationTarget.close();
     this.loading_ringsTarget.classList.add("hidden");
   }
 
+  // ボタンなどにアクションを配置し、loading_animationを開き、loading_ringsを表示する
+  // すこしディレイをかけることで、軽いローディングの場合はすこしブラーがかかる程度のエフェクトのみにする
   show() {
     this.loading_ringsTarget.classList.add("hidden");
     this.loading_animationTarget.showModal();
