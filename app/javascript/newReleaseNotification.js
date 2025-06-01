3f35cc5fd7bf0c315d6f20c3723e98ae3a893247
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-06-01 13:39:59 +0900

    fix: nil回避を追加し、表示する期間の制限を設けました

--- app/javascript/newReleaseNotification.js
+++ app/javascript/newReleaseNotification.js
@@ -2,22 +2,24 @@ export function newReleaseNotification() {
   const newReleaseNotification= document.querySelector("#new_release_notification");
   const newReleaseClose= document.querySelector("#new_release_close");
 
-  if (!newReleaseNotification) {
-    console.log('addButtonNotification: id="new_release_notification"の要素が見つかりません');
-    return;
-  }
-
   if (newReleaseClose && newReleaseNotification) {
     newReleaseClose.addEventListener("click", function() {
       localStorage.setItem('newReleaseNotification', 'closed');
       newReleaseNotification.style.display = "none";
     });
+  }else{
+    console.log('newReleaseNotification: id="new_release_notification または id="new_release_close"の要素が見つかりません');
+  }
+
+  if (!newReleaseNotification) {
+    console.log('newReleaseNotification: id="new_release_notification"の要素が見つかりません');
+    return;
   }
 
   const newReleaseNotificationClose = localStorage.getItem('newReleaseNotification');
   if (newReleaseNotificationClose === 'closed') {
     newReleaseNotification.style.display = "none";
-  }else {
+  }else{
     newReleaseNotification.style.display = "flex";
   }
 }
