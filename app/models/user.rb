9603f7016a5fe6e28619611f37bf0bc5974b2224
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-06-24 22:33:18 +0900

    fix: バグが起きた日に改修した分を戻しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,4 +49,32 @@ class User < ApplicationRecord
   def completed_tasks_hidden?
     is_hide_completed_tasks == true
   end
+
+  # Lineログイン用の設定
+  def social_profile(provider)
+    social_profiles.select { |sp| sp.provider == provider.to_s }.first
+  end
+
+  # rubocop:disable Naming/AccessorMethodName
+  def set_values(omniauth)
+    return if provider.to_s != omniauth["provider"].to_s || uid != omniauth["uid"]
+
+    credentials = omniauth["credentials"]
+    info = omniauth["info"]
+
+    # rubocop:disable Lint/UselessAssignment
+    access_token = credentials["refresh_token"]
+    access_secret = credentials["secret"]
+    credentials = credentials.to_json
+    name = info["name"]
+    # rubocop:enable Lint/UselessAssignment
+  end
+
+  # rubocop:disable Style/RedundantSelf
+  def set_values_by_raw_info(raw_info)
+    self.raw_info = raw_info.to_json
+    self.save!
+  end
+  # rubocop:enable Naming/AccessorMethodName
+  # rubocop:enable Style/RedundantSelf
 end
