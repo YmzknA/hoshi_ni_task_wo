711d2e8671e1dfe3c60d86746d0c502ff1284737
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-07-25 14:01:26 +0900

    docs: UserInitializationConcernのrespond_to?チェックに説明コメントを追加

--- app/controllers/concerns/user_initialization_concern.rb
+++ app/controllers/concerns/user_initialization_concern.rb
@@ -4,6 +4,8 @@ module UserInitializationConcern
   private
 
   def initialize_new_user(user)
+    # Deviseのresourceは必ずしもUserモデルとは限らないため、respond_to?でチェック
+    # また、nilや異なるオブジェクトが渡される可能性も考慮
     return unless user.respond_to?(:new_user?) && user.new_user?
 
     UserRegistration::MakeTasksMilestones.create_tasks_and_milestones(user)
